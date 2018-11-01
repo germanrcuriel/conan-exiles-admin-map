244a76290b34b1472b75dab7c5d9a2b49636c3fb
germanrcuriel <germix@germix.net>
2018-11-01 14:32:36 +0100

    Publish new filters api routes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,14 +1,23 @@
 const apiRoutes = (app) => {
 
   require('./all')(app)
+  require('./altars')(app)
+  require('./animalpens')(app)
   require('./beds')(app)
   require('./buildings')(app)
   require('./campfires')(app)
+  require('./chests')(app)
   require('./crabpots')(app)
+  require('./crafting')(app)
   require('./fishnets')(app)
+  require('./maprooms')(app)
   require('./pets')(app)
+  require('./pippi/all')(app)
   require('./pippi/thespians')(app)
   require('./players')(app)
+  require('./trebuchets')(app)
+  require('./vaults')(app)
+  require('./waterwells')(app)
   require('./wheelsofpain')(app)
 
 }
