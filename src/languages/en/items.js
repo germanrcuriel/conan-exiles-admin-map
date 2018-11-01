0811d2ff9388c8ae91889ef3f1ab7187a7f3b6e7
germanrcuriel <germix@germix.net>
2018-11-01 14:31:58 +0100

    Add new translations for ui and items

--- src/languages/en/items.js
+++ src/languages/en/items.js
@@ -1,28 +1,169 @@
 export default {
-  "/Game/Systems/Building/Placeables/BP_PL_Bed_Single_Stygian.BP_PL_Bed_Single_Stygian_C": "Bed - Stygian",
-  "/Game/Systems/Building/Placeables/BP_PL_Bedroll_Clean.BP_PL_Bedroll_Clean_C": "Rawhide Bedroll",
-  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_WheelOfPain.BP_PL_CraftingStation_WheelOfPain_C": "Lesser Wheel of Pain",
-  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_WheelOfPainXL.BP_PL_CraftingStation_WheelOfPainXL_C": "Wheel of Pain",
-  "/Game/Systems/Building/Placeables/BP_PL_Crafting_CampFire_Big.BP_PL_Crafting_CampFire_Big_C": "Large Campfire",
-  "/Game/Systems/Building/Placeables/BP_PL_Bed_Double.BP_PL_Bed_Double_C": "Double bed",
+
+  // Altar
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Derketo.BP_PL_Altar_Derketo_C": "Pleasure Place of Derketo",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Derketo_T2.BP_PL_Altar_Derketo_T2_C": "Pleasure Dome of Derketo",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Derketo_T3.BP_PL_Altar_Derketo_T3_C": "Pleasure Palace of Derketo",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Set.BP_PL_Altar_Set_C": "Sepulcher of Set",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Set_T2.BP_PL_Altar_Set_T2_C": "Altar of Set",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Set_T3.BP_PL_Altar_Set_T3_C": "Sanctum of Set",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Yog.BP_PL_Altar_Yog_C": "Pit of Yog",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Yog_T2.BP_PL_Altar_Yog_T2_C": "Rift of Yog",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Yog_T3.BP_PL_Altar_Yog_T3_C": "Abyss of Yog",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Mitra.BP_PL_Altar_Mitra_C": "Shrine of Mitra",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Mitra_T2.BP_PL_Altar_Mitra_T2_C": "Sanctuary of Mitra",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Mitra_T3.BP_PL_Altar_Mitra_T3_C": "Temple of Mitra",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Ymir.BP_PL_Altar_Ymir_C": "Altar of Ymir",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Ymir_T2.BP_PL_Altar_Ymir_T2_C": "Blooded Altar of Ymir",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_Ymir_T3.BP_PL_Altar_Ymir_T3_C": "Exalted Altar of Ymir",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_JhebbalSag.BP_PL_Altar_JhebbalSag_C": "Bower of JhebbalSag",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_JhebbalSag_T2.BP_PL_Altar_JhebbalSag_T2_C": "Retreat of JhebbalSag",
+  "/Game/Systems/Building/Placeables/BP_PL_Altar_JhebbalSag_T3.BP_PL_Altar_JhebbalSag_T3_C": "Grove of JhebbalSag",
+
+  // Animal Pen
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Station_AnimalPen_Tier_1.BP_PL_Crafting_Station_AnimalPen_Tier_1_C": "Animal Pen",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Station_AnimalPen_Tier_2.BP_PL_Crafting_Station_AnimalPen_Tier_2_C": "Reinforced Animal Pen",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Station_AnimalPen_Tier_3.BP_PL_Crafting_Station_AnimalPen_Tier_3_C": "Armored Animal Pen",
+
+  // Bed
   "/Game/Systems/Building/Placeables/BP_PL_Bed_Common.BP_PL_Bed_Common_C": "Bed",
-  "/Game/Systems/Building/Placeables/BP_PL_Crafting_CrabPot.BP_PL_Crafting_CrabPot_C": "Shellfish Trap",
-  "/Game/Systems/Building/Placeables/BP_PL_Crafting_FishNet.BP_PL_Crafting_FishNet_C": "Fish Trap",
-  "/Game/DLC/DLC_Aquilonia/Blueprints/BP_PL_Bed_Aquilonian.BP_PL_Bed_Aquilonian_C": "Ornate Single Bed",
+  "/Game/Systems/Building/Placeables/BP_PL_Bed_Double.BP_PL_Bed_Double_C": "Double bed",
   "/Game/Systems/Building/Placeables/BP_PL_Bed_Single_Folded.BP_PL_Bed_Single_Folded_C": "Bed - Folded",
-  "/Game/Systems/Building/Placeables/BP_PL_Crafting_CampFire.BP_PL_Crafting_CampFire_C": "Campfire",
+  "/Game/Systems/Building/Placeables/BP_PL_Bed_Single_Stygian.BP_PL_Bed_Single_Stygian_C": "Bed - Stygian",
   "/Game/Systems/Building/Placeables/BP_PL_Bed_Single_Stygian2.BP_PL_Bed_Single_Stygian2_C": "Bed - Stygian",
-  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_WheelOfPainXXL.BP_PL_CraftingStation_WheelOfPainXXL_C": "Greater Wheel of Pain",
-  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Bonfire.BP_PL_Crafting_Bonfire_C": "Bonfire",
+  "/Game/Systems/Building/Placeables/BP_PL_Bed_Demo.BP_PL_Bed_Demo_C": "Nordheimer Bed",
+  "/Game/DLC/DLC_Aquilonia/Blueprints/BP_PL_Bed_Aquilonian.BP_PL_Bed_Aquilonian_C": "Ornate Single Bed",
+
+  // Bedroll
   "/Game/Systems/Building/Placeables/BP_PL_Bedroll_Fiber.BP_PL_Bedroll_Fiber_C": "Fiber Bedroll",
-  "/Game/Systems/Building/BP_BuildFoundation_T3.BP_BuildFoundation_T3_C": "Reinforced Stone Building",
-  "/Game/Systems/Building/BP_HL_BuildFoundation_T3.BP_HL_BuildFoundation_T3_C": "Black Ice-Reinforced Wooden Building",
+  "/Game/Systems/Building/Placeables/BP_PL_Bedroll_Clean.BP_PL_Bedroll_Clean_C": "Rawhide Bedroll",
+
+  // Beehive
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Beehive.BP_PL_Crafting_Beehive_C": "Beehive",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Beehive_Improved.BP_PL_Crafting_Beehive_Improved_C": "Improved Beehive",
+
+  // Bench
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Armor.BP_PL_CraftingStation_Armor_C": "Armorer's Bench",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Armor2.BP_PL_CraftingStation_Armor2_C": "Improved Armorer's Bench",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Metal.BP_PL_CraftingStation_Metal_C": "Blacksmith's Bench",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Metal2.BP_PL_CraftingStation_Metal2_C": "Improved Blacksmith's Bench",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Wood.BP_PL_CraftingStation_Wood_C": "Carpenter's Bench",
+
+  // Building
   "/Game/Systems/Building/BP_BuildFoundation.BP_BuildFoundation_C": "Sandstone Building",
   "/Game/Systems/Building/BP_BuildTriangleFoundation.BP_BuildTriangleFoundation_C": "Sandstone Building",
-  "/Game/DLC/DLC_Khitai/Blueprints/BP_BuildFoundation_T3_Khitai.BP_BuildFoundation_T3_Khitai_C": "Khitan Building",
   "/Game/Systems/Building/BP_BuildFoundation_T2.BP_BuildFoundation_T2_C": "Stonebrick Building",
+  "/Game/Systems/Building/BP_BuildTriangleFoundation_T2.BP_BuildTriangleFoundation_T2_C": "Stonebrick Building",
+  "/Game/Systems/Building/BP_BuildFoundation_T3.BP_BuildFoundation_T3_C": "Reinforced Stone Building",
+  "/Game/Systems/Building/BP_BuildTriangleFoundation_T3.BP_BuildTriangleFoundation_T3_C": "Reinforced Stone Building",
+  "/Game/Systems/Building/BP_HL_BuildFoundation_T2.BP_HL_BuildFoundation_T2_C": "Insulated Wooden Building",
+  "/Game/Systems/Building/BP_HL_BuildTriangleFoundation_T2.BP_HL_BuildTriangleFoundation_T2_C": "Insulated Wooden Building",
+  "/Game/Systems/Building/BP_HL_BuildFoundation_T3.BP_HL_BuildFoundation_T3_C": "Black Ice-Reinforced Wooden Building",
+  "/Game/Systems/Building/BP_HL_BuildTriangleFoundation_T3.BP_HL_BuildTriangleFoundation_T3_C": "Black Ice-Reinforced Wooden Building",
+  "/Game/DLC/DLC_Khitai/Blueprints/BP_BuildFoundation_T3_Khitai.BP_BuildFoundation_T3_Khitai_C": "Khitan Building",
+  "/Game/DLC/DLC_Khitai/Blueprints/BP_BuildTriangleFoundation_T3_Khitai.BP_BuildTriangleFoundation_T3_Khitai_C": "Khitan Building",
+  "/Game/DLC/DLC_Aquilonia/Blueprints/BP_BuildFoundation_T3_Aquilonia.BP_BuildFoundation_T3_Aquilonia_C": "Aquilonian Building",
+  "/Game/DLC/DLC_Aquilonia/Blueprints/BP_BuildTriangleFoundation_T3_Aquilonia.BP_BuildTriangleFoundation_T3_Aquilonia_C": "Aquilonian Building",
   "/Game/DLC/DLC_Pict/Blueprints/BP_BuildFoundation_T3_Pict.BP_BuildFoundation_T3_Pict_C": "Frontier Building",
   "/Game/DLC/DLC_Pict/Blueprints/BP_BuildTriangleFoundation_T3_Pict.BP_BuildTriangleFoundation_T3_Pict_C": "Frontier Building",
-  "/Game/DLC/DLC_Aquilonia/Blueprints/BP_BuildFoundation_T3_Aquilonia.BP_BuildFoundation_T3_Aquilonia_C": "Aquilonian Building",
-  "/Game/Systems/Building/BP_HL_BuildTriangleFoundation_T2.BP_HL_BuildTriangleFoundation_T2_C": "Insulated Wooden Building",
+
+  // Campfire
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_CampFire.BP_PL_Crafting_CampFire_C": "Campfire",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_CampFire_Big.BP_PL_Crafting_CampFire_Big_C": "Large Campfire",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Bonfire.BP_PL_Crafting_Bonfire_C": "Bonfire",
+
+  // Chest
+  "/Game/Systems/Building/Placeables/BP_PL_Chest_Large_Cupboard.BP_PL_Chest_Large_Cupboard_C": "Cupboard",
+  "/Game/Systems/Building/Placeables/BP_PL_Chest_Medium.BP_PL_Chest_Medium_C": "Wooden Box",
+  "/Game/Systems/Building/Placeables/BP_PL_Chest_Large.BP_PL_Chest_Large_C": "Large Chest",
+
+  // Compost Heap
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Compost.BP_PL_Crafting_Compost_C": "Compost Heap",
+
+  // Dryer
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Dryer.BP_PL_CraftingStation_Dryer_C": "Dryer",
+
+  // Fermentation barrel
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Fermenter.BP_PL_CraftingStation_Fermenter_C": "Fermentation Barrel",
+
+  // Firebowl Cauldron
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Mixer.BP_PL_CraftingStation_Mixer_C": "Firebowl Cauldron",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Mixer2.BP_PL_CraftingStation_Mixer2_C": "Improved Firebowl Cauldron",
+
+  // Fish and shellfish trap
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_FishNet.BP_PL_Crafting_FishNet_C": "Fish Trap",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_CrabPot.BP_PL_Crafting_CrabPot_C": "Shellfish Trap",
+
+  // Fluid press
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Presser.BP_PL_Crafting_Presser_C": "Fluid Press",
+
+  // Furnace
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Furnace.BP_PL_CraftingStation_Furnace_C": "Furnace",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Furnace2.BP_PL_CraftingStation_Furnace2_C": "Improved Furnace",
+
+  // Grinder
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Grinder.BP_PL_Crafting_Grinder_C": "Grinder",
+
+  // Map room
+  "/Game/Systems/Building/Placeables/Maproom/BP_PL_Maproom.BP_PL_Maproom_C": "Map room",
+
+  // Pippi
+  "/Game/Mods/Pippi/Pippi_Egress.Pippi_Egress_C": "Pippi - Egress",
+  "/Game/Mods/Pippi/Pippi_Glorb.Pippi_Glorb_C": "Pippi - Glorb",
+  "/Game/Mods/Pippi/Pippi_LootSpawner.Pippi_LootSpawner_C": "Pippi - LootSpawner",
+  "/Game/Mods/Pippi/Pippi_Mob.Pippi_Mob_C": "Pippi - Thespian",
+  "/Game/Mods/Pippi/Pippi_Note.Pippi_Note_C": "Pippi - Note",
+  "/Game/Mods/Pippi/Pippi_NPCSpawner.Pippi_NPCSpawner_C": "Pippi - NPCSpawner",
+  "/Game/Mods/Pippi/Pippi_Warper.Pippi_Warper_C": "Pippi - Warper",
+  "/Game/Mods/Pippi/Pippi_Zohne.Pippi_Zohne_C": "Pippi - Zohne",
+
+  // Planter
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Planter.BP_PL_Crafting_Planter_C": "Crude Planter",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Planter_T2.BP_PL_Crafting_Planter_T2_C": "Planter",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Planter_T3.BP_PL_Crafting_Planter_T3_C": "Improved Planter",
+
+  // Preservation box
+  "/Game/Systems/Building/Placeables/BP_PL_Icebox.BP_PL_Icebox_C": "Preservation box",
+  "/Game/Systems/Building/Placeables/BP_PL_Icebox_Improved.BP_PL_Icebox_Improved_C": "Improved Preservation box",
+
+  // Signpost
+  "/Game/Systems/Building/Placeables/BP_PL_Signpost.BP_PL_Signpost_C": "Wooden Signpost",
+  "/Game/Systems/Building/Placeables/BP_PL_WallSign.BP_PL_WallSign_C": "Wall Sign",
+
+  // Stove
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Stove.BP_PL_Crafting_Stove_C": "Stove",
+  "/Game/Systems/Building/Placeables/BP_PL_Crafting_Stove2.BP_PL_Crafting_Stove2_C": "Improved Stove",
+
+  // Tannery
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Tannery.BP_PL_CraftingStation_Tannery_C": "Tannery",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_Tannery2.BP_PL_CraftingStation_Tannery2_C": "Improved Tannery",
+
+  // Tent
+  "/Game/Systems/Building/Placeables/BP_PL_SurvivalistTent.BP_PL_SurvivalistTent_C": "Simple Tent",
+
+  // Trebuchet
+  "/Game/Systems/Building/Placeables/Trebuchet/Trebuchet_V2/BP_V2_Trebuchet_Foundation.BP_V2_Trebuchet_Foundation_C": "Siege Foundation",
+  "/Game/Systems/Building/Placeables/Trebuchet/Trebuchet_V2/BP_V2_Trebuchet_Base.BP_V2_Trebuchet_Base_C": "Trebuchet Base",
+  "/Game/Systems/Building/Placeables/Trebuchet/Trebuchet_V2/BP_V2_Trebuchet_Base_Frame.BP_V2_Trebuchet_Base_Frame_C": "Trebuchet Frame",
+  "/Game/Systems/Building/Placeables/Trebuchet/Trebuchet_V2/BP_V2_Trebuchet_Complete.BP_V2_Trebuchet_Complete_C": "Trebuchet",
+
+  // Vault
+  "/Game/Systems/Building/Placeables/BP_PL_Chest_Vault.BP_PL_Chest_Vault_C": "Vault",
+
+  // Water well
+  "/Game/Systems/Building/Placeables/BP_PL_Water_Well_Tier2.BP_PL_Water_Well_Tier2_C": "Water Well",
+  "/Game/Systems/Building/Placeables/BP_PL_Water_Well_Large.BP_PL_Water_Well_Large_C": "Large Water Well",
+  "/Game/Systems/Building/Placeables/BP_PL_Water_Well_MitraStatue.BP_PL_Water_Well_MitraStatue_C": "Statue of Refreshment",
+
+  // Wheel of pain
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_WheelOfPain.BP_PL_CraftingStation_WheelOfPain_C": "Lesser Wheel of Pain",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_WheelOfPainXL.BP_PL_CraftingStation_WheelOfPainXL_C": "Wheel of Pain",
+  "/Game/Systems/Building/Placeables/BP_PL_CraftingStation_WheelOfPainXXL.BP_PL_CraftingStation_WheelOfPainXXL_C": "Greater Wheel of Pain",
+
+  // Worktable
+  "/Game/Systems/Building/Placeables/BP_PL_WorkStation_Artisan.BP_PL_WorkStation_Artisan_C": "Artisan's Worktable",
+  "/Game/Systems/Building/Placeables/BP_PL_WorkStation_Artisan_Khitai.BP_PL_WorkStation_Artisan_Khitai_C": "Khitan Artisan Table",
+  "/Game/Systems/Building/Placeables/BP_PL_WorkStation_Artisan_Aquilonia.BP_PL_WorkStation_Artisan_Aquilonia_C": "Aquilonian Artisan Table",
+  "/Game/Systems/Building/Placeables/BP_PL_WorkStation_Slaver.BP_PL_WorkStation_Slaver_C": "Torturer's Worktable"
+
 }
